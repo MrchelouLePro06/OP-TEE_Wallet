0ded5e342d0a16b8164df8979b80cb34f5f54c4f
PBismuth <mohamed.younes9153@gmail.com>
2026-02-19 23:10:35 +0100

    Storage data

--- optee_examples/storage_data/host/main.c
+++ optee_examples/storage_data/host/main.c
@@ -11,6 +11,7 @@ void usage(const char *app_name) {
     fprintf(stderr, "  %s add <nom_doc> <contenu>\n", app_name);
     fprintf(stderr, "  %s get <nom_doc>\n", app_name);
     fprintf(stderr, "  %s delete <nom_doc>\n", app_name);
+    fprintf(stderr, "  %s list\n", app_name); // Ajout du list
     exit(1);
 }
 
@@ -41,32 +42,35 @@ int main(int argc, char *argv[]) {
         op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT, TEEC_MEMREF_TEMP_INPUT,
                                          TEEC_NONE, TEEC_NONE);
         
-        op.params[0].tmpref.buffer = argv[2]; // Nom du fichier
-        op.params[0].tmpref.size = strlen(argv[2]);
-        op.params[1].tmpref.buffer = argv[3]; // Contenu
-        op.params[1].tmpref.size = strlen(argv[3]);
+        op.params[0].tmpref.buffer = argv[2]; 
+        op.params[0].tmpref.size = strlen(argv[2]) + 1; // +1 pour le '\0'
+        op.params[1].tmpref.buffer = argv[3]; 
+        op.params[1].tmpref.size = strlen(argv[3]) + 1; // +1 pour le '\0'
 
         res = TEEC_InvokeCommand(&sess, CMD_ADD_DOCUMENT, &op, &err_origin);
         if (res == TEEC_SUCCESS) printf("SUCCESS: Document added\n");
+        else printf("ERROR: Failed to add document (0x%x)\n", res);
     }
 
     // --- CAS : GET DOCUMENT ---
     else if (strcmp(argv[1], "get") == 0) {
         if (argc != 3) usage(argv[0]);
 
-        char read_buf[4096]; // Buffer de sortie pour le PDF/Doc
+        char read_buf[4096]; 
         op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT, TEEC_MEMREF_TEMP_OUTPUT,
                                          TEEC_NONE, TEEC_NONE);
         
-        op.params[0].tmpref.buffer = argv[2]; // Nom à chercher
-        op.params[0].tmpref.size = strlen(argv[2]);
+        op.params[0].tmpref.buffer = argv[2]; 
+        op.params[0].tmpref.size = strlen(argv[2]) + 1;
         op.params[1].tmpref.buffer = read_buf;
-        op.params[1].tmpref.size = sizeof(read_buf);
+        op.params[1].tmpref.size = sizeof(read_buf) - 1; // On garde 1 octet pour le '\0' final
 
         res = TEEC_InvokeCommand(&sess, CMD_GET_DOCUMENT, &op, &err_origin);
         if (res == TEEC_SUCCESS) {
-            read_buf[op.params[1].tmpref.size] = '\0'; // Fin de chaîne pour printf
-            printf("CONTENT:%s\n", read_buf);
+            read_buf[op.params[1].tmpref.size] = '\0'; 
+            printf("CONTENT: %s\n", read_buf);
+        } else {
+            printf("ERROR: Failed to get document (0x%x)\n", res);
         }
     }
 
@@ -76,10 +80,28 @@ int main(int argc, char *argv[]) {
 
         op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT, TEEC_NONE, TEEC_NONE, TEEC_NONE);
         op.params[0].tmpref.buffer = argv[2];
-        op.params[0].tmpref.size = strlen(argv[2]);
+        op.params[0].tmpref.size = strlen(argv[2]) + 1;
 
         res = TEEC_InvokeCommand(&sess, CMD_DELETE_DOCUMENT, &op, &err_origin);
         if (res == TEEC_SUCCESS) printf("SUCCESS: Document deleted\n");
+        else printf("ERROR: Failed to delete document (0x%x)\n", res);
+    }
+
+    // --- CAS : LIST DOCUMENTS ---
+    else if (strcmp(argv[1], "list") == 0) {
+        if (argc != 2) usage(argv[0]);
+
+        char list_buf[4096];
+        op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE, TEEC_NONE, TEEC_NONE);
+        op.params[0].tmpref.buffer = list_buf;
+        op.params[0].tmpref.size = sizeof(list_buf);
+
+        res = TEEC_InvokeCommand(&sess, CMD_LIST_DOCUMENTS, &op, &err_origin);
+        if (res == TEEC_SUCCESS) {
+            printf("DOCUMENTS: %s\n", list_buf);
+        } else {
+            printf("ERROR: Failed to list documents (0x%x)\n", res);
+        }
     }
 
     else {
--- optee_examples/storage_data/ta/include/storage_data_ta.h
+++ optee_examples/storage_data/ta/include/storage_data_ta.h
@@ -5,10 +5,11 @@
 
 #define TA_STORAGE_DATA_UUID \
 	{ 0x0dc9979e, 0xc403, 0x46b1, \
-		{ 0x80, 0xd7, 0x75, 0xad, 0x52, 0x0b, 0xc1, 0x1e} }//0dc9979e-c403-46b1-80d7-75ad520bc11e
+		{ 0x80, 0xd7, 0x75, 0xad, 0x52, 0x0b, 0xc1, 0x1e} }
 
 #define CMD_ADD_DOCUMENT       20
 #define CMD_GET_DOCUMENT       21
 #define CMD_DELETE_DOCUMENT    22	
+#define CMD_LIST_DOCUMENTS     23
 
 #endif /*TA_STORAGE_DATA_H*/
